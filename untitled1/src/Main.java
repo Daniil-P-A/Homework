10517edad094abefe10bda5bcdb3c99e739be265
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-06 04:17:31 +0100

    Homework_05.03.2023_first_part

---
+++ untitled1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean BerlinIsTheCapitalOfGermany = true;
+        boolean TokioIsTheCapitalOfPoland = false;
+
+        int NumberOfDaysInAYear = 365;
+
+        double NormalHumanTemperature = 36.6;
+
+        String anyName = "Daniil";
+
+        System.out.println(anyName + " said he jumped " + NumberOfDaysInAYear + " times, but it was " +
+                        TokioIsTheCapitalOfPoland + ", it wasn't " + BerlinIsTheCapitalOfGermany + ". " +
+                        "Actually he jumped " + NormalHumanTemperature + " times.");
+
+
+
+    }
+}
