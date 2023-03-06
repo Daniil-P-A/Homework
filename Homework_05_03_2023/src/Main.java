f3c470cc196c98538a8cdd6232143411cc2ab24b
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-06 01:06:09 +0100

    Add files via upload

---
+++ Homework_05_03_2023/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String FlyToTheMoon = ("Время полета на Луну: ");
+        String FlyToTheMars = ("Приблизительное время полета на Марс: ");
+
+        int TimeOfFlyToTheMoon = 78;
+        int TimeOfFlyToTheMars = 3879;
+
+        boolean maybe = true;
+        boolean OrNot = false;
+
+        System.out.println(FlyToTheMoon + TimeOfFlyToTheMoon + " часов. " + FlyToTheMars + TimeOfFlyToTheMars + " часов. " +
+                "Сможет ли Илон Маск отправить черовека на Марс? " + maybe + " or " + OrNot + "? А как думаешь ТЫ?!" );
+
+
+
+    }
+}
