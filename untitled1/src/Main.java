49cdd0ad0f53118efe62b11943a38a3b54e65bf1
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-06 05:09:52 +0100

    Homework_06.03.2023_second_part

--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -1,20 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-
-        boolean BerlinIsTheCapitalOfGermany = true;
-        boolean TokioIsTheCapitalOfPoland = false;
-
-        int NumberOfDaysInAYear = 365;
-
-        double NormalHumanTemperature = 36.6;
-
-        String anyName = "Daniil";
-
-        System.out.println(anyName + " said he jumped " + NumberOfDaysInAYear + " times, but it was " +
-                        TokioIsTheCapitalOfPoland + ", it wasn't " + BerlinIsTheCapitalOfGermany + ". " +
-                        "Actually he jumped " + NormalHumanTemperature + " times.");
-
-
-
+        add(); divided(); third(); multiplication();
     }
+        public static void add() {
+
+        int Ten = 10;
+        int OneHundred = 100;
+        int result1 = Ten + OneHundred;
+
+        System.out.println(result1);
+    }
+        public static void divided() {
+
+            double OneHundred2 = 100;
+            double Thirty = 30;
+            double result2 = OneHundred2 / Thirty;
+
+            System.out.println(result2);
+        }
+
+        public static void third() {
+
+        double number1 = 0.000001;
+        double number2 = 24.999999;
+        double number3 = 50;
+        double result3 = number1 + number2 - number3;
+
+        System.out.println(result3);
+        }
+
+        public static void multiplication() {
+
+        int num1 = 10;
+        int num2 = 30;
+        int result4 = num1 * num2;
+
+            System.out.println(result4);
+        }
+
+
+
 }
+
+
+
+
+
+
+
+
+
