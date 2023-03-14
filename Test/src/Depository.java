e193a866015a650fba6d2c8fc49ef4604af17bb1
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-14 18:15:12 +0100

    Add files via upload

---
+++ Test/src/Depository.java
@@ -0,0 +1,9 @@
+public class Depository {
+
+    public static int pencil = 1000;
+    public static int pen = 2000;
+    public static int rubber = 500;
+    public static String pinball = "400";
+
+
+}
