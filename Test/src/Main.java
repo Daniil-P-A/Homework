6b77ea515849c295704896069a400c3ffe8cca5c
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-14 18:06:02 +0100

    Homework14_03_2023 Калькулятор
    Калькулятор который переводит ЛЮБОЕ 4-х значное число из 16-ричной в 10-чную систему

---
+++ Test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Depository.pencil);
+
+        System.out.println(Depository.pen);
+
+        System.out.println(Depository.rubber);
+
+        System.out.println(Depository.pinball);
+    }
+}
