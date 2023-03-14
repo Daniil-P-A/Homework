ea33fe8acefa1fc52a32514dc17f086110b6131c
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-14 18:16:15 +0100

    Delete Test directory

--- Test/src/Depository.java
+++
@@ -1,9 +0,0 @@
-public class Depository {
-
-    public static int pencil = 1000;
-    public static int pen = 2000;
-    public static int rubber = 500;
-    public static String pinball = "400";
-
-
-}
