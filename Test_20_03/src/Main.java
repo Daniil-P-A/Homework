eb77fd1b181ded4630c033a9bd40a068136c19a4
Daniil-P-A <125497979+Daniil-P-A@users.noreply.github.com>
2023-03-21 17:32:46 +0100

    Add files via upload

---
+++ Test_20_03/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Planet Mercury = new Planet("Mercury", "3.3 * 10^23 kg", "6.083 * 10 km^3", "4.5 billion years");
+
+        Planet Venus = new Planet("Venus", "4.867 * 10^24 kg", "108,2 billion km", "4.504 billion years");
+
+        Planet Ears = new Planet("Ears", "5.972 * 10^24kg","149.6 million km", " 4.543 billion years" );
+
+        Planet Mars = new Planet("Mars", "6.39 * 10^23kg", "227 million km", "4.6 billion years");
+
+        Planet Jupiter = new Planet("Jupiter", "1.8 * 10^27kg", "740 millio km", "4.6 billion years");
+
+
+
+
+
+
+    }
+}
---
+++ Test_20_03/src/Planet.java
@@ -0,0 +1,9 @@
+public class Planet {
+
+    public Planet(String planetName, String weight, String distanceFromTheSun, String age){
+        System.out.println("\nИмя планеты: " + planetName + "\nВес: " + weight + "\nРасстояние до солнца: " + distanceFromTheSun + "\nВозраст: " + age);
+    };
+    }
+
+
+
